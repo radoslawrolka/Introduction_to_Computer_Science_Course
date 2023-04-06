5eb52a6741d3beb78014a4794f28983879881194
Radosław Rolka <128408457+radoslawrolka@users.noreply.github.com>
2023-04-06 16:47:33 +0200

    Delete zestaw_7 directory

--- zestaw_7/z24_d.py
+++
@@ -1,6 +0,0 @@
-"""
-24. Dana jest lista, który zakończona jest cyklem.
-Napisać funkcję, która zwraca liczbę elementów przed cyklem.
-"""
-
-# XD co? przecież to jest to samo co 23
--- zestaw_7/z35_d.py
+++
@@ -1,7 +0,0 @@
-"""
-35. A teraz proszę rozwiązać wszystkie powyższe zadania przy założeniu że
-listy zawierają wartownika.
-"""
-
-
-# pojebalo
